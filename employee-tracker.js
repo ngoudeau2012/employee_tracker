1f68d48c365eaa7339a69f082ea76c34cb68412f
Nicholas Goudeau <ngoudeau2012@gmail.com>
2020-05-28 18:38:49 -0700

    fully functioning application. Need to add a readme before final publication

--- employee-tracker.js
+++ employee-tracker.js
@@ -156,77 +156,167 @@ function addEmployee(){
 };
 
 function employeesByDepartment(){
-    allDepartments()
-    inquirer.prompt({
-
-    })
-    let query = "SELECT * FROM role"
-    connection.query(query, function(err,res){
-        if(err) throw err;
-        console.table(res);
-        start();
-    })
+    connection.query("SELECT * FROM department", function(err,res){
+        if (err) throw err;
+        inquirer.prompt({
+            type: "list",
+            name: "department_id",
+            message: "Which department would you like to select from?",
+            choices: res.map((department) => {
+                return {
+                    name: department.name,
+                    value: department.id
+                };
+            })
+        }).then(function(res){
+            console.log(res);
+            let query = "SELECT table1.id, first_name,last_name,title FROM employees AS table1 LEFT JOIN role AS table2 ON table1.role_id = table2.id WHERE table2.department_id = ?;"
+            connection.query(query,[res], function(err,res){
+                if (err) throw err;
+                console.table(res);
+                start();
+            });
+        });
+    });
+    
+    
 };
 
-// function addEmployee() {
-//     connection.query("Select title, id FROM role", function (errRole, resRole) {
-//       if (errRole) throw errRole;
-//       connection.query(
-//         `SELECT CONCAT(first_name," ",last_name) AS ManagerName, id FROM employee;`,
-//         function (errManager, resManager) {
-//           if (errManager) throw errManager;
-//           inquirer
-//             .prompt([
-//               {
-//                 name: "employeeFirst",
-//                 type: "input",
-//                 message: "Please enter employee's first name.",
-//               },
-//               {
-//                 name: "employeeLast",
-//                 type: "input",
-//                 message: "Please enter employee's last name.",
-//               },
-//               {
-//                 name: "employeeRole",
-//                 type: "list",
-//                 message: "Please select employee's role.",
-//                 choices: resRole.map((role) => {
-//                   return {
-//                     name: role.title,
-//                     value: role.id,
-//                   };
-//                 }),
-//               },
-//               {
-//                 name: "employeeManager",
-//                 type: "list",
-//                 message: "Please select employee's manager.",
-//                 choices: resManager.map((manager) => {
-//                   return {
-//                     name: manager.ManagerName,
-//                     value: manager.id,
-//                   };
-//                 }),
-//               },
-//             ])
-//             .then(function (answer) {
-//               connection.query(
-//                 "INSERT INTO employee SET ?",
-//                 {
-//                   first_name: answer.employeeFirst,
-//                   last_name: answer.employeeLast,
-//                   role_id: answer.employeeRole,
-//                   manager_id: answer.employeeManager,
-//                 },
-//                 function (err) {
-//                   if (err) throw err;
-//                   console.log("New employee added!");
-//                   start();
-//                 }
-//               );
-//             });
-//         }
-//       );
-//     });
-//   }
+function employeesByManager(){
+    connection.query("SELECT id, CONCAT(first_name,' ',last_name) AS name FROM employees", function(err, res){
+        if (err) throw err;
+        inquirer.prompt({
+            type: "list",
+            name: "manager_id",
+            message: "Which manager would you like to search by?",
+            choices: res.map((employee) => {
+                return {
+                    name: employee.name,
+                    value: employee.id
+                };
+            })
+        }).then(function(res){
+            console.log(res);
+            let query = "SELECT table1.id, table1.first_name, table1.last_name FROM employees AS table1 LEFT JOIN employees AS table2 ON table1.manager_id = table2.id WHERE table1.?;"
+            connection.query(query,[res], function(err,res){
+                if (err) throw err;
+                console.table(res);
+                start();
+            });
+        });
+    });
+};
+
+function removeEmployee(){
+    connection.query("SELECT id, CONCAT(first_name,' ',last_name) AS name FROM employees", function(err, res){
+        if (err) throw err;
+        inquirer.prompt({
+            type: "list",
+            name: "id",
+            message: "Who's information would you like to remove from the company?",
+            choices: res.map((employee) => {
+                return {
+                    name: employee.name,
+                    value: employee.id
+                };
+            })
+        }).then(function(res){
+            let query = "DELETE FROM employees WHERE ?"
+            connection.query(query,[res], function(err,res){
+                if (err) throw err;
+                console.table("Great! " + res.affectedRows + " employee was deleted");
+                start();
+            });
+        });
+    });
+};
+
+function updateRole(){
+    // Run role query to gather possible roles available
+    connection.query("SELECT title, id FROM role", function(errRole, resRole){
+        if (errRole) throw errRole;
+        // Run employee query to gather possible managers available
+        connection.query("SELECT id, CONCAT(first_name,' ',last_name) AS name FROM employees", function(errEmp, resEmp){
+            if(errEmp) throw errEmp;
+            inquirer.prompt([
+                {
+                    type: "list",
+                    name: "id",
+                    message: "Who's role would you like to update?",
+                    choices: resEmp.map((employee) => {
+                        return {
+                            name: employee.name,
+                            value: employee.id
+                        };
+                    })
+                },
+                {
+                    type: "list",
+                    name: "role_id",
+                    message: "What is this employee's new role?",
+                    choices: resRole.map((role) => {
+                        return {
+                            name: role.title,
+                            value: role.id
+                        };
+                    })
+                }
+                ]).then(function(res){
+                    // console.log(res);
+                    connection.query(
+                        "UPDATE employees SET role_id = ? Where id = ?",
+                        [res.role_id, res.id],
+                        function (err,res){
+                            if (err) throw err
+                            console.log("The employee's role was updated! " + res.affectedRows + " row changed.")
+                            start();
+                        });
+                });
+        });
+    });
+};
+
+function updateManager(){
+    connection.query("SELECT id, CONCAT(first_name,' ',last_name) AS name FROM employees", function(errEmp, resEmp){
+        if(errEmp) throw errEmp;
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "id",
+                message: "Who's manager would you like to update?",
+                choices: resEmp.map((employee) => {
+                    return {
+                        name: employee.name,
+                        value: employee.id
+                    };
+                })
+            },
+            {
+                type: "list",
+                name: "manager_id",
+                message: "Who is this employee's new manager?",
+                choices: resEmp.map((manager) => {
+                    return {
+                        name: manager.name,
+                        value: manager.id
+                    };
+                })
+            }
+            ]).then(function(res){
+                // console.log(res);
+                connection.query(
+                    "UPDATE employees SET manager_id = ? Where id = ?",
+                    [res.manager_id, res.id],
+                    function (err,res){
+                        if (err) throw err
+                        console.log("The employee's manager was updated! " + res.affectedRows + " row changed.")
+                        start();
+                    });
+            });
+    });
+}
+
+function exit(){
+    connection.end
+    console.log("You've ended your session, please enter 'node employee-tracker.js' in the command line to start another session.")
+};
